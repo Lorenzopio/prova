60ac50abe86e759beb9a30b3def0f641f782d813
Lorenzopio <lorenzops02@gmail.com>
2020-10-08 18:00:56 +0200

    esercizioPari

---
+++ es.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std; 
+int main (){
+	
+	int numero;
+	
+	cout<<"inserisci un numero"<<endl;
+	cin>>numero;
+	
+	
+	
+	if(numero%2==0){
+		
+		cout<<"il numero e' pari"<<endl;
+		
+	}
+	
+	if (numero%2!=0){
+		
+		cout<<"il numero e' dispari"<<endl;
+		
+	}
+	
+	
+}
